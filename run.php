3a454acc27015e0b560514e79028cbd7b723a8b8
Adrien Roches <adrien@roc-it.tech>
2024-10-08 16:53:31 +0200

    Update run.php to test __toString

--- run.php
+++ run.php
@@ -2,6 +2,7 @@
 
 require_once __DIR__ . '/src/CanAuthenticate.php';
 require_once __DIR__ . '/src/MemberLevel.php';
+require_once __DIR__ . '/src/User.php';
 require_once __DIR__ . '/src/Member.php';
 require_once __DIR__ . '/src/Admin.php';
 
@@ -14,10 +15,23 @@ $member5 = new Member('member5', 'member5', 'member5', 10);
 $admin1 = new Admin('admin1', 'admin1', 'admin1', 10, MemberLevel::Admin);
 $admin2 = new Admin('admin2', 'admin2', 'admin2', 10, MemberLevel::SuperAdmin);
 
+echo '--------------------------------' . PHP_EOL;
 echo "Member count : " . Member::count() . PHP_EOL;
 echo "Admin count : " . Admin::count() . PHP_EOL;
 
 unset($member1);
 
+echo '    >>> After unset $member1 <<<' . PHP_EOL;
 echo "Member count : " . Member::count() . PHP_EOL;
 echo "Admin count : " . Admin::count() . PHP_EOL;
+echo '--------------------------------' . PHP_EOL;
+
+echo PHP_EOL;
+
+echo '--------------------------------' . PHP_EOL;
+echo 'Trying out __toString()' . PHP_EOL;
+
+echo "Member 2 : {$member2}" . PHP_EOL;
+echo "Admin 1 : {$admin1}" . PHP_EOL;
+echo "Admin 2 : {$admin2}" . PHP_EOL;
+echo '--------------------------------' . PHP_EOL;
