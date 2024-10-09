b995c49aa5f58e3505150f65cb0d71f673a18a42
Adrien Roches <adrien@roc-it.tech>
2024-10-09 11:48:07 +0200

    USe composer as autoloader

--- run.php
+++ run.php
@@ -1,11 +1,6 @@
 <?php
 
-require_once __DIR__ . '/src/BadCredentialsException.php';
-require_once __DIR__ . '/src/CanAuthenticate.php';
-require_once __DIR__ . '/src/MemberLevel.php';
-require_once __DIR__ . '/src/User.php';
-require_once __DIR__ . '/src/Member.php';
-require_once __DIR__ . '/src/Admin.php';
+require_once __DIR__ . '/vendor/autoload.php';
 
 use App\Admin;
 use App\BadCredentialsException;
